fdeb126f228bc8f8073cbe0058b8c9b66c47e0fd
Nicolás Lobos <lobos.nicolas2@gmail.com>
2021-09-10 09:47:07 -0300

    arreglo getGifs y https

--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(category) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=GItBZrLgwVFWYhG4rl2B28iSJwCwDU5K&q=${ encodeURI(category) }&limit=4`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=GItBZrLgwVFWYhG4rl2B28iSJwCwDU5K&q=${ encodeURI(category) }&limit=4`;
     const resp = await fetch (url);
     const { data } = await resp.json();
     const gifs = data.map( img => {
